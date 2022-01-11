283e4b2cd2eb3c75e171c3417772d5a2bc593ee2
Martin Cho <zoveloper@gmail.com>
2022-01-11 01:02:14 -0500

    connect frontend google login function  with sanity backend

--- socialmemory_frontend/src/components/Login.jsx
+++ socialmemory_frontend/src/components/Login.jsx
@@ -4,9 +4,26 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import { client } from "../client";
 
 const Login = () => {
-  const responseGoogle = (response) => {};
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+
+    const { name, googleId, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
   return (
     <div className="flex jutify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
