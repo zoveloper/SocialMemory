667edb93e5dcd7a8b066fecd0d469890a1973922
Martin Cho <zoveloper@gmail.com>
2022-01-12 23:53:48 -0500

    Added Side Navbar Menu Link and Active function

--- socialmemory_frontend/src/components/Sidebar.jsx
+++ socialmemory_frontend/src/components/Sidebar.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill, RihomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import logo from "../assets/logo.png";
+
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 text-grey-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+const isActiveStyle =
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 boarder-black  transition-all duration-200 ease-in-out capitalize";
+
+const Sidebar = (user, closeToggle) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 item-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+          >
+            <RiHomeFill />
+            home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            Discover categories
+          </h3>
+        </div>
+      </div>
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
