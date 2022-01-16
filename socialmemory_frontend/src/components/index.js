9a4b512f06ebf304534229deb2d16e31125da60b
Martin Cho <zoveloper@gmail.com>
2022-01-15 23:54:35 -0500

    linked all the components to routes

--- socialmemory_frontend/src/components/index.js
+++ socialmemory_frontend/src/components/index.js
@@ -5,3 +5,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Feed } from "./Feed";
 export { default as PinDetail } from "./PinDetail";
 export { default as CreatePin } from "./CreatePin";
+export { default as Search } from "./Search";
